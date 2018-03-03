1531f460dfa394aad9059464221e28066dac39b3
wesdriver45 <wesdriver45@gmail.com>
2018-03-03 10:48:53 -0500

    additional files

---
+++ client/components/Actual.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import {Button} from 'react-bootstrap';
+// import Modal from 'react-modal';
+// import axios from 'axios';
+// import {Link} from 'react-router-dom';
+// var querystring = require('querystring');
+
+// render()
+
+// export default Actual;
