434230fb6a85623c81c0cb78dd596d1539e169cb
Robyn Harka <reharka@gmail.com>
2018-03-03 10:51:22 -0500

    mongo

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
-mongoose.connect('mongodb://heroku_phq7520:admin123@ds151508.mlab.com:51508/heroku_phqt7520');
+mongoose.connect('mongodb://reharka%40gmail.com:TSar0428@ds151508.mlab.com:51508/heroku_phqt7520');
 
 app.use('/', router);
 
-module.exports=app;
+module.exports = app;
