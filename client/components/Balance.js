9a8c0f1798f65601f93aa8c83984093ca74559fd
wesdriver45 <wesdriver45@gmail.com>
2018-03-04 15:25:47 -0500

    added react

--- client/components/Balance.js
+++ client/components/Balance.js
@@ -5,6 +5,18 @@
 // import {Link} from 'react-router-dom';
 // var querystring = require('querystring');
 
+// class Balance extends React.Component {
+//     constructor() {
+//           super();
+//     this.state = {
+//             description: '',
+//             amount: '',
+//             month: '',
+//             year: '',
+//             messageFromServer: '',
+//             modalIsOpen: false
+//           }
+
 // render()
 
 // export default Balance;
