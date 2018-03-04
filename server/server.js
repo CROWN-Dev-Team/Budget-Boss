15e8de301f11eda12733a947450cdbb50eb8411e
wesdriver45 <wesdriver45@gmail.com>
2018-03-04 16:32:02 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
@@ -13,8 +14,24 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
-mongoose.connect('mongodb://reharka%40gmail.com:TSar0428@ds151508.mlab.com:51508/heroku_phqt7520');
-
 app.use('/', router);
+
+var db = process.env.MONGODB_URI || 'mongodb://admin:admin123@ds255308.mlab.com:55308/heroku_1tgc4d2n';
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+mongoose.connect(db, function(eror) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("mongoose connection successful");
+    }
+});
+
+app.listen(PORT, function() {
+    console.log('running at localhost: ' + PORT);
+   });
 
 module.exports = app;
