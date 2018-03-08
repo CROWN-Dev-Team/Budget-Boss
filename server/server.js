dc04fc742f2a28ee33124851c9e9d0f54a17d1d3
Robyn Harka <reharka@gmail.com>
2018-03-07 19:17:48 -0500

    files

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 app.use('/', router);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/powerful-fortress-11248";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fast-brook-41384";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
     // useMongoClient: true
