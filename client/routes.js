c5735ac319090071451046b404a6c8e1e9ba321a
Robyn Harka <reharka@gmail.com>
2018-03-13 18:16:25 -0400

    app update

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 //client/routes.js
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import App from './components/app';
+import App from './components/App';
 export const Routes = () => (
     <Switch>
       <Route exact path='/' component={App} />
--- server/server.js
+++ server/server.js
@@ -27,3 +27,5 @@ app.listen(PORT, function() {
    });
 
 module.exports = app;
+
+
