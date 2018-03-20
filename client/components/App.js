787279d2a9935feccc93efd057352a38f3ff41c2
Robyn Harka <reharka@gmail.com>
2018-03-20 14:42:49 -0400

    css update

--- client/components/App.js
+++ client/components/App.js
@@ -8,6 +8,8 @@ import Delete from './Delete';
 import { Tab, Tabs } from 'react-bootstrap';
 import YearTabsRouter from './tabs/yearTabsRouter';
 import MonthTabs from './tabs/monthTabs';
+import '../css/App.css';
+
 export default class App extends React.Component {
 constructor() {
     super();
